010b022d73e9e1a736d43416cfe22e34f4979f20
talessantos1989 <talesdealmeida@gmail.com>
2023-03-16 17:35:15 -0300

    log

--- NumerologiaCabalistica.Repository/CommandRepository.cs
+++ NumerologiaCabalistica.Repository/CommandRepository.cs
@@ -16,6 +16,7 @@ namespace NumerologiaCabalistica.Repository
 				using (MySqlConnection conn = new MySqlConnection(connectionString))
 				{
 					conn.Open();
+					Console.WriteLine("conexao sucesso");
 					MySqlCommand command = new MySqlCommand($"SELECT id_cliente, nome, email, telefone, data_compra, data_nascimento, codigo_transacao from clientes where " +
 						$"enviado = @enviado and data_compra < @dataDeHoje", conn);
 
