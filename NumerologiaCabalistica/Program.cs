33f5e9733deb223fab1ef82497a11758ea799f9a
talessantos1989 <talesdealmeida@gmail.com>
2023-03-20 18:51:22 -0300

    log

--- NumerologiaCabalistica/Program.cs
+++ NumerologiaCabalistica/Program.cs
@@ -1,11 +1,10 @@
 
 using Microsoft.EntityFrameworkCore;
-using MySql.Data.MySqlClient;
 using NumerologiaCabalistica.Calculos;
 using NumerologiaCabalistica.Calculos.Interfaces;
 using NumerologiaCabalistica.Data;
-using NumerologiaCabalistica.Repository;
 using NumerologiaCabalistica.Service;
+using Pomelo.EntityFrameworkCore.MySql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 //string connectionString = "server=containers-us-west-57.railway.app;database=railway;uid=root;pwd=Ma3GXkBCE2hU5LiCKGgV;port=5993";
 
 string connectionString = builder.Configuration.GetConnectionString("NumerologiaCabalisticaConnection");
-
+Console.WriteLine(connectionString);
 //Adiciona o entity framework na aplicação
 builder.Services.AddDbContext<NumerologiaCabalisticaDbContext>(
-    opts => opts.UseLazyLoadingProxies().UseMySql(connectionString));
-// ServerVersion.AutoDetect(connectionString)
-
+    opts => opts.UseLazyLoadingProxies().UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 
